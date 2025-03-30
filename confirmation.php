b7bee81c5972fc55523e88968199ea963cf3be7c
CarteregaloApp <issaabdoulazizou.bj@gmail.com>
2025-03-30 13:07:38 +0100

    Update confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -75,6 +75,15 @@
         function retour() {
             window.location.href = "traitement.php";
         }
-    </script>
+    
+
+
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement({pageLanguage: 'fr'}, 'google_translate_element');
+    }
+
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 </body>
 </html>
